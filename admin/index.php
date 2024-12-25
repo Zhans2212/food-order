55c8e653e74bcd8752b108717a868243e7d8a35a
Zhans2212 <gansayat2001@gmail.com>
2024-12-25 10:08:34 +0500

    first commit

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<html lang="en">
+    <head>
+        <title>Food Order Website - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            <div class="wrapper">
+                Menu Goes here
+            </div>
+        </div>
+        <!-- Menu Section Ends -->
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                Main Content Goes here
+            </div>
+        </div>
+        <!-- Main Content Section Ends -->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2024 All rights reserved, Some Restaurant, Developed by - <a href="">Nyx</a></p>
+            </div>
+
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
